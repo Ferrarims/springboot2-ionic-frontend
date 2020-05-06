991063e2b64442ddc4eb7d0e23454b36fb22deda
Fernando Ferrari <ferrarims85@gmail.com>
2020-05-06 15:02:20 +0100

    Mostrando as categorias

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,3 +1,4 @@
 export const API_CONFIG = {
-    baseUrl: "http://localhost:8080"
+    baseUrl: "http://localhost:8080",
+    bucketBaseUrl: "https://springboot2-ionic.s3-eu-west-1.amazonaws.com"
 }
